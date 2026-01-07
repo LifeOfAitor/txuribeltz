09df7130eb815de0431fb541cc1dbe407b3052f1
aitorzubiri <aitorgaillard99@gmail.com>
2026-01-07 09:28:31 +0100

    log in and sign up front is done for now

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace txuribeltz
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+        /*
+         * Sisteman logeatuko gara hemendik
+         * Behar dugu:
+         *  - erabiltzailea
+         *  - pasahitza
+         * Eta ez badago erabiltzailerik, sortzeko aukera sortu
+        */
+        public void login(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // zerbitzarira konektatu eta erabiltzaile honekin logeatu
+
+                if (string.IsNullOrWhiteSpace(txtUsuario.Text) || string.IsNullOrWhiteSpace(txtPassword.Password))
+                {
+                    txt_erroreak.Text = "Erabiltzaile edo pasahitza hutsik daude.";
+                    return;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                // erabiltzailea sortzeko menua eman
+
+                txt_erroreak.Text = $"Sortu erabiltzailea, ez dago {txtUsuario} erabiltzailerik";
+            }
+        }
+
+        /*
+         * Sisteman erabiltzaile berri bat sortuko dugu hemendik
+         * Behar dugu:
+         *  - erabiltzailea
+         *  - pasahitza
+        */
+        private void newUser(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // zerbitzarira konektatu eta erabiltzaile hau sortu
+                Window signup = new SingUp();
+                signup.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                txt_erroreak.Text = $"Ezin izan da erabiltzailerik sortu";
+            }
+        }
+    }
+}
---
+++ SingUp.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace txuribeltz
+{
+    /// <summary>
+    /// Interaction logic for SingUp.xaml
+    /// </summary>
+    public partial class SingUp : Window
+    {
+        public SingUp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistratu_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // zerbitzarira konektatu eta erabiltzaile hau sortu
+                if (string.IsNullOrWhiteSpace(txtErabiltzailea.Text) || string.IsNullOrWhiteSpace(txtPasahitza.Password))
+                {
+                    txt_erroreak.Text = "Erabiltzaile edo pasahitza hutsik daude.";
+                    return;
+                }
+                if (txtPasahitza.Password != txtPasahitza2.Password)
+                {
+                    txt_erroreak.Text = "Pasahitzak ez datoz bat.";
+                    return;
+                }
+                // erabiltzailea sortu
+                MessageBox.Show("Erabiltzailea ongi erregistratu da.");
+                Close();
+            }
+            catch (Exception ex)
+            {
+                // errorea erakutsi
+                txt_erroreak.Text = $"Errorea erregistratzean: {ex.Message}";
+            }
+        }
+
+        private void btnAtzera_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
